4209d0a2928201fe7683a97fcb0a23a2a62223d2
kali <kali@kali.kali.com>
2025-07-15 18:23:19 +0700

    menu

--- Frontend/ProjectCourse/src/component/Header.jsx
+++ Frontend/ProjectCourse/src/component/Header.jsx
@@ -4,6 +4,8 @@ import menuIcon from '../assets/menu_24dp_000000_FILL0_wght400_GRAD0_opsz24.svg'
 import { useState } from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
+import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 function Header() {
     const [isActive, setIsActive] = useState(false);
@@ -12,7 +14,14 @@ function Header() {
             <ul className={`sidebar ${isActive ? "active" : ""}`}>
 
                 <li><a href="#">Coding</a></li>
-                <li><a href="">Explore</a></li>
+                <li className='dropdownMenu'><a href="">Explore</a>
+                    <ul><li><a href="">num1 </a></li>
+                        <li><a href="">num 2</a></li>
+                        <li><a href="">num 3</a></li>
+                        <li><a href="">num 4</a></li>
+                        <li><a href="">num 5</a></li>
+                    </ul>
+                </li>
                 <li><a href="">Place</a></li>
                 <li><a href="">search</a></li>
                 <li><a href="">login</a></li>
@@ -20,7 +29,14 @@ function Header() {
             </ul>
             <ul>
                 <li className='hide'><a href="#">Home</a></li>
-                <li className='hide'><a href="">Explore</a></li>
+                <li className='hide dropdownMenu'><div className='ExploreArrow' ><a href="">Explore</a><a href=""><FontAwesomeIcon className='aggleRight' icon={faAngleRight} /><FontAwesomeIcon className='aggleDown' icon={faAngleDown} /></a></div>
+                    <ul><li><a href="">num1 </a></li>
+                        <li><a href="">num 2</a></li>
+                        <li><a href="">num 3</a></li>
+                        <li><a href="">num 4</a></li>
+                        <li><a href="">num 5</a></li>
+                    </ul>
+                </li>
                 <li className='searchbar hide'><div className='search-container'><input type="text" placeholder='search' />
                     <a href=""><FontAwesomeIcon icon={faMagnifyingGlass} /></a></div></li>
                 <li className='hide'><a href="">search</a></li>
