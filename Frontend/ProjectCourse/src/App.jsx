7785a2edc12060475a4c5e972741050afa983f05
Nguyen Tu Hoang <nguyentuhoang0711@gmail.com>
2025-07-21 11:08:10 +0700

    Hoang

--- Frontend/ProjectCourse/src/App.jsx
+++ Frontend/ProjectCourse/src/App.jsx
@@ -1,35 +1,10 @@
-import RegisterStudent from "./Student/Register/RegisterStudent"
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
-import Headers from "./component/Header"
-import Footer from "./component/Footer"
-import Login from "./Login/Login"
-import Home from "./Home/Home"
-import ForgotPassword from "./ForgotPassword/ForgotPassword"
-import Register from "./Register/Register"
-import RegisterInstructor from "./instructor/Register/RegisterInstructor"
-import MathPage from "./Student/Pages/Math/Math"
-import VerifyEmail from "./component/VerrifyEmail"
+import HomeCourses from "./instructor/HomeInstructor/HomeCourses";
+import { Routes, Route } from "react-router-dom";
 function App() {
-
-
   return (
-    <>
-
-
-
-      <Routes>
-        <Route path="/" element={<RegisterStudent />} />
-        <Route path="/Login" element={<Login />} />
-        <Route path="/verify" element={<VerifyEmail />} />
-
-        <Route path="/Home" element={<Home />} />
-      </Routes>
-
-
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<HomeCourses />} />
+    </Routes>
+  );
 }
-//  {/* <Route path="/Login" element={<Login />}></Route> */}
-//     <Route path="/Home" element={<Home />} />
-//     <Route path="/RegisterStudent" element={<RegisterStudent />} />
-export default App
+export default App;
